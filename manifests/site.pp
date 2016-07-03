985fc7990289e692e8394dc7511da0f13f3379fd
OiSiS <oisis@o2.pl>
2016-07-03 23:31:13 +0200

    Added replacepkgs to yum command, extra development packages

--- manifests/site.pp
+++ manifests/site.pp
@@ -191,8 +191,10 @@ node 'rpm.tomekw.pl' {
   yum::group { 'Development Tools':
     ensure => present,
   }
+  $enhancers = [ "yum-utils", ]
+  package { $enhancers: ensure => "installed" }
   exec { 'install_remi_yum_repo':
-    command => '/usr/bin/rpm -ihv http://rpms.famillecollet.com/enterprise/remi-release-7.rpm',
+    command => '/usr/bin/rpm -ihv --replacepkgs http://rpms.famillecollet.com/enterprise/remi-release-7.rpm',
   }
 }
 
